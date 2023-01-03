7dd09e57c5751f1b3cd313edf809e9aa838f7e1a
qwa1822 <asdzxc1822@naver.com>
2023-01-03 14:07:41 +0900

    'ex01문제해결'

--- ex01/1.js
+++ ex01/1.js
@@ -1,4 +1,36 @@
-let array1 = ["a", "b", "c"];
-let array2 = ["a", "b", "c"];
+function charCount(str) {
+  let obj = {};
+  for (let char of str) {
+    char = char.toLowerCase();
+    if (isAlphaNuemric(char)) {
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+}
 
-console.log(array1.concat(array2));
+function Charcount2(str) {
+  let hell = {};
+  for (let char of str) {
+    char = char.toLowerCase();
+    if (/[a-z0-9]/.test(str)) {
+      hell[char] = ++hell[char] || 1;
+    }
+  }
+  return hell;
+}
+
+function isAlphaNuemric(char) {
+  let code = char.charCodeAt(0);
+
+  if (
+    !(code > 47 && code < 58) &&
+    !(code > 64 && code < 96) &&
+    !(code > 96 && code < 123)
+  ) {
+    return false;
+  }
+  return true;
+}
+console.log(charCount("Hithereeeeeee"));
+console.log(Charcount2("eweqeqweqeqeqeqeqweq"));
