5851776ce14ff6da1d29ea8aa6b519a7aefd353d
qwa1822 <asdzxc1822@naver.com>
2023-01-06 01:03:39 +0900

    '정렬'

---
+++ 정렬/합병정렬/ex01.js
@@ -0,0 +1,49 @@
+function merge(arr1,arr2){
+    let result=[];
+  let i=0;
+  let j=0;
+
+
+  while(i<arr1.length && j<arr2.length){
+    if(arr2[j]>arr1[i]){
+      result.push(arr1[i]);
+      i++;
+    } else{
+      result.push(arr2[j]);
+    }
+
+    while(i<arr1.length){
+      result.push(arr1[i]);
+      i++;
+    }
+    while(j<arr2.legnth){
+      result.push(arr2[j])
+      j++;
+    }
+
+    
+    return result;
+
+  }
+
+
+}
+
+
+
+function mergeSort(arr){
+  if(arr.length<=1) return arr;
+  let mid=Math.floor(arr.length/2);
+  let left=mergeSort(arr.slice(0,mid));
+  let right=mergeSort(arr.slice(mid));
+
+  return merge(left,right);
+
+
+
+  
+
+}
+
+
+console.log(merge([1,10,50],[2,14,99,100]));
